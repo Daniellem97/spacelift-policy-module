228f2ea263cefe0b65c8be754706c03815ed7095
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-09-02 23:25:04 +0200

    Update main.tf

--- spacelift-policy-module/first-stack/main.tf
+++ spacelift-policy-module/first-stack/main.tf
@@ -19,3 +19,15 @@ resource "spacelift_policy" "login_policy" {
   type     = "LOGIN"
   body     = "package spacelift"
 }
+
+variable "spacelift_first_account_key_id" {
+  description = "API Key ID for the first Spacelift account"
+  type        = string
+  sensitive   = true
+}
+
+variable "spacelift_first_account_key_secret" {
+  description = "API Key Secret for the first Spacelift account"
+  type        = string
+  sensitive   = true
+}
