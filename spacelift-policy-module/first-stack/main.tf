b533e76303765b65d14f092a46fd436bd9630291
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-09-02 23:24:07 +0200

    Update main.tf

--- spacelift-policy-module/first-stack/main.tf
+++ spacelift-policy-module/first-stack/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "1.15.0"  # Make sure this matches the desired provider version
+    }
+  }
+}
+provider "spacelift" {
+  alias            = "first-stack"
+  api_key_endpoint = "https://gtestlogin.app.spacelift.io"
+  api_key_id       = var.spacelift_first_account_key_id
+  api_key_secret   = var.spacelift_first_account_key_secret
+}
 provider "spacelift" {
   alias = "first-stack"
 }
